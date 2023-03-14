a4aa0511ff7fade81c3c97d23f5242443d3506f1
fkolender <hedrite@hotmail.com>
2023-03-13 23:26:37 -0300

    initial commit

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  app.enable('trust proxy');
+  res.json({ 'ipaddress': req.ip, 'language': req.language, 'software': req.software });
 });
 
 // listen for requests :)
